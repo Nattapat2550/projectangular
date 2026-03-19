e10457ff86b57da95a85a96a75671d6437383a6f
Nattapat2550 <nyansungvon@gmail.com>
2026-03-19 20:34:12 +0700

    text

--- backend/test/auth.e2e-spec.ts
+++ backend/test/auth.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 // จำลองคลาสส่งอีเมลเพื่อไม่ให้ส่งอีเมลจริงๆ ตอนรันเทส
